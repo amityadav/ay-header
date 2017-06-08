03fc007009bba2675d9b3b4a85ea4bbc3bc92eb8
AMIT <amit.yadav19@gmail.com>
2017-06-08 01:11:11 -0400

    Final header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class AmitHeaderComponent implements OnInit {
+  public headerTitle:any;
 
   constructor() { }
 
   ngOnInit() {
+    this.headerTitle = 'This is a sample header from Amit Yadav!!!';
   }
 
 }
+
+
+
